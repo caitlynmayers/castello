1f398a1f88679d09ee3d073ea95ebe4591118164
caitlynmayers <caitlynmcarney@gmail.com>
2014-09-28 10:36:06 -0400

    more header stylings on the contained text, adding search icon svg.

--- header.php
+++ header.php
@@ -48,6 +48,25 @@
 					
 					</nav>
 					
+					<div class="search">
+					
+						<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
+							<g>
+								<path fill="#E7E7E7" d="M20,0C9,0,0,9,0,20s9,20,20,20s20-9,20-20S31,0,20,0z M27,28.7l-2.8-2.8c-1.3,0.9-2.9,1.4-4.5,1.4
+									c-4.4,0-7.9-3.6-7.9-7.9s3.6-7.9,7.9-7.9s7.9,3.6,7.9,7.9c0,1.6-0.5,3.1-1.3,4.4l2.8,2.8L27,28.7z"/>
+								<path fill="#E7E7E7" d="M19.7,13.8c-3.1,0-5.6,2.5-5.6,5.6s2.5,5.6,5.6,5.6c3.1,0,5.6-2.5,5.6-5.6S22.8,13.8,19.7,13.8z"/>
+							</g>
+						</svg>
+					
+					</div>
+					
 				</div>
+				
+				<section class="masthead">
+				
+					<h1>MD/P<span>h</span>D student, Neuroscientist, Husband, Lover of Technology (carbon & silicon-based varieties), Avid Learner, Passionate Advocate.</h1>
+				
+				</section>
 				
 			</header>
