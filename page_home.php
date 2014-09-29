56e171da622f0d724c1c146ca56a0d71852ea015
caitlynmayers <caitlynmcarney@gmail.com>
2014-09-28 22:09:37 -0400

    reworking background styles to apply to body, creating home template and adding background styles to content section.

---
+++ page_home.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+Template Name: Home Page
+*/
+get_header(); ?>
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<?php $this_page = new Post(get_the_ID()); ?>
+<div id="main" role="main">
+	<div class="content-wrap">
+		<section>
+			<?php the_content(); ?>
+		</section>
+	</div>
+</div>
+<?php endwhile; endif; ?>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
