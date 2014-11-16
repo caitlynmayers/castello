a1843e92d116e5dfd95ea7e4ef38270afa704270
caitlynmayers <caitlynmcarney@gmail.com>
2014-11-15 21:35:39 -0500

    adding matter single template

---
+++ single-matter.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+Template Name: Interior Page
+*/
+get_header( 'interior' ); ?>
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<?php $this_page = new Post(get_the_ID()); ?>
+<div id="main" role="main">
+	<div class="content-wrap">
+		<article class="content">
+			<?php the_content(); ?>
+		</article>
+		<?php get_sidebar(); ?>
+	</div>
+</div>
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
